6c28ad55c15f7862faabba2af6305c4df387d660
Wandygans <87267241+Wandygans@users.noreply.github.com>
2023-03-17 17:22:47 +0700

    Update wandy.js

--- wandy.js
+++ wandy.js
@@ -123,6 +123,19 @@ bio = `${bio_nya.status}`
 } catch {
 bio = `-`
 }
+	
+function runtime(seconds) {
+	seconds = Number(seconds);
+	var d = Math.floor(seconds / (3600 * 24));
+	var h = Math.floor(seconds % (3600 * 24) / 3600);
+	var m = Math.floor(seconds % 3600 / 60);
+	var s = Math.floor(seconds % 60);
+	var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+	var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+	var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+	var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+	return dDisplay + hDisplay + mDisplay + sDisplay;
+}
 
 const wibT = wib()
 const witaT = wita()
@@ -143,6 +156,32 @@ randomnya = gambar[Math.floor(Math.random() * gambar.length)]
 resultnya = randomnya.result
 return resultnya
 }
+
+const used = process.memoryUsage()
+const cpus = os.cpus().map(cpu => {
+cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+return cpu
+})
+const cpu = cpus.reduce((last, cpu, _, { length }) => {
+last.total += cpu.total
+last.speed += cpu.speed / length
+last.times.user += cpu.times.user
+last.times.nice += cpu.times.nice
+last.times.sys += cpu.times.sys
+last.times.idle += cpu.times.idle
+last.times.irq += cpu.times.irq
+return last
+}, {
+speed: 0,
+total: 0,
+times: {
+user: 0,
+nice: 0,
+sys: 0,
+idle: 0,
+irq: 0
+}
+})
 
 let d = new Date
 let locale = 'id'
@@ -196,8 +235,17 @@ anuy = `
 ┌──⭓ *Main Menu*
 │
 │⭔ ${prefix}menu
+│⭔ ${prefix}ping
 │
-└───────⭓`
+└───────⭓
+
+┌──⭓ *Main Menu*
+│
+│⭔ >
+│⭔ $
+│
+└───────⭓
+`
 
 //━━━━━━━━━━━━━━━[ COMMAND ]━━━━━━━━━━━━━━━━━//
  
@@ -217,6 +265,27 @@ buttons: buttonss,
 headerType: 4
 }
 conn.sendMessage(m.chat, buttonMzessage,  { ephemeralExpiration: 604800 }, { quoted: m, mentions: m.key.participant })
+break
+		case 'ping': case 'botstatus': case 'statusbot': {
+let timestamp = speed()
+let latensi = speed() - timestamp
+let all = require('util').inspect(hit.all)
+respon = `
+Kecepatan Respon ${latensi.toFixed(4)} _Second_ \n\nRuntime : ${runtime(process.uptime())}
+
+💻 Info Server
+RAM: ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
+
+_NodeJS Memory Usaage_
+${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${formatp(used[key])}`).join('\n')}
+
+${cpus[0] ? `_Total CPU Usage_
+${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
+_CPU Core(s) Usage (${cpus.length} Core CPU)_
+${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
+`.trim()
+m.reply(respon)
+}
 break
 
 default:
